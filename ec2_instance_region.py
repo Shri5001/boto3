82a8cdc6af51268341037ea7a4f80a52fe3d94da
shri <shriyeole5001@gmail.com>
2024-10-07 11:37:35 +0530

    instance creaation by using boto3

---
+++ ec2_instance_region.py
@@ -0,0 +1,20 @@
+import boto3
+client=boto3.client('ec2', region_name="us-east-1")
+response=client.run_instances(
+    ImageId="ami-0fff1b9a61dec8a5f",
+    InstanceType="t2.micro",
+    KeyName="love",
+    MaxCount=1,
+    MinCount=1,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'MyEC2Instance'
+                }
+            ]
+        }
+    ]
+)
